8e7056623b976124b9a9f688ba101b9ea1e2ccae
Sualty <lagouge@polytech.unice.fr>
2016-07-12 15:37:17 +0200

    graphe

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
-        GetDataFromUnidrive getter = new GetDataFromUnidrive("http://129.111.0.136/US/1.21/dynamic/readparval.xml");
+      /*  GetDataFromUnidrive getter = new GetDataFromUnidrive("http://129.111.0.136/US/1.21/dynamic/readparval.xml");
         String response = getter.getResponse();
 
         ParserXML parser = new ParserXML(response);
@@ -12,5 +14,12 @@ public class Main {
         String nom = parser.getNom_param();
         String val = parser.getVal_param();
         System.out.println(nom + " "+ val);
+*/
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(-3);
+        arr.add(10);
+        DrawGraph graph = new DrawGraph(arr);
+        graph.createAndShowGui();
     }
 }
