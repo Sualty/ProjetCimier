8dc95ececec23d2cf6efc0e69b3279eaa09b59c9
Sualty <lagouge@polytech.unice.fr>
2016-07-18 09:22:50 +0200

    passage à maven

--- src/main/java/GetDataFromUnidrive.java
+++ src/main/java/GetDataFromUnidrive.java
@@ -1,8 +1,13 @@
-import java.io.*;
-import java.net.Socket;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-
 /**
  * This class opens a socket for sending a request to the Unidrive SP .
  */
@@ -50,14 +55,22 @@ public class GetDataFromUnidrive {
      * @return the response of the Unidrive
      * @throws IOException
      */
+    private final String USER_AGENT = "Mozilla/5.0";
     public String getResponse() throws IOException {
-        String result="";
+/*        String result="";
         Socket socket = new Socket(host,port);
+
+        //encoding login and password
+        String username = "root";
+        String password = "ut72";
+        String auth = username + ":" + password;
+        String encodedAuth = Base64.encode(auth.getBytes());
 
         //sending request
         PrintWriter request = new PrintWriter( socket.getOutputStream() );
         request.print(  "GET " + path + " HTTP/1.1\r\n" +
                 "Host: " + host + "\r\n" +
+                "Authorization: Basic " + encodedAuth + "\r\n" +
                 "Connection: close\r\n\r\n");
         request.flush( );
 
@@ -69,6 +82,26 @@ public class GetDataFromUnidrive {
         while ((line = rd.readLine()) != null) {
             result = result+"\n"+line;
         }
+        return result;*/
+        String url = "http://192.168.130.182/US/4.02/dynamic/readparval.xml";
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+
+        HttpResponse response = client.execute(request);
+
+        System.out.println("Response Code : "
+                + response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+      /*  String result = "";
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result+=line;
+        }*/
+        String result = response.getEntity().toString();
         return result;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,23 @@
-import javax.swing.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
-      /*  GetDataFromUnidrive getter = new GetDataFromUnidrive("http://129.111.0.136/US/1.21/dynamic/readparval.xml");
+        GetDataFromUnidrive getter = new GetDataFromUnidrive("http://192.168.130.182/US/4.02/dynamic/readparval.xml");
         String response = getter.getResponse();
-
-        ParserXML parser = new ParserXML(response);
+        System.out.println(response);
+    /*    ParserXML parser = new ParserXML(response);
         parser.processLineByLine();
         String nom = parser.getNom_param();
         String val = parser.getVal_param();
         System.out.println(nom + " "+ val);
-*/
+
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(1);
         arr.add(-3);
         arr.add(10);
         DrawGraph graph = new DrawGraph(arr);
-        graph.createAndShowGui();
+        graph.createAndShowGui();*/
     }
 }
